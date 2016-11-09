f96a69db84b0315ab22a2d31026f595857ed7aad
Tyler Fowler <tylerfowler.1337@gmail.com>
2016-11-09 16:18:29 -0600

    Add initial coreos key schemas, basic validation

---
+++ builtin/providers/template/datasource_coreos_cloudinit.go
@@ -0,0 +1,186 @@
+package template
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func dataSourceCoreOSCloudinit() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceCoreOSCloudinitRead,
+
+		// TODO: add users, write-files sections
+		Schema: map[string]*schema.Schema{
+			"use_shebang": &schema.Schema{
+				Description: "Whether or not to use the shebang (`#` vs `#!`) in the #cloud-config directive",
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+			},
+			"hostname": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"ssh_authorized_keys": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"manage_etc_hosts": &schema.Schema{
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: etcHostsValidation,
+			},
+			"etcd": etcdSchema,
+		},
+	}
+}
+
+func dataSourceCoreOSCloudinitRead(dat *schema.ResourceData, meta interface{}) error {
+	// TODO: implement
+	return nil
+}
+
+// Validation functions
+
+// etcHostsValidation runs validation on the cloud-config's `manage_etc_hosts` key,
+// currently CoreOS only supports a value of "localhost", so throw a warning when any other
+// value is given
+func etcHostsValidation(val interface{}, key string) (warnings []string, _ []error) {
+	value := val.(string)
+	if value != "localhost" {
+		warnings = append(warnings, "Manage etc hosts currently only supports values of 'localhost' in CoreOS")
+	}
+
+	return
+}
+
+// CoreOS Key schemas
+
+// etcdSchema maps to coreos: etcd
+// TODO: add remaining configuration options
+var etcdSchema = &schema.Schema{
+	Type:       schema.TypeMap,
+	Optional:   true,
+	Deprecated: "The etcd block has been deprecated by CoreOS in favor of etcd2",
+	Elem: &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"name":      &schema.Schema{Type: schema.TypeString, Required: true},
+			"discovery": &schema.Schema{Type: schema.TypeString, Optional: true},
+			"addr":      &schema.Schema{Type: schema.TypeString, Optional: true},
+			"peer_addr": &schema.Schema{Type: schema.TypeString, Optional: true},
+		},
+	},
+}
+
+// etcd2Schema maps to coreos: etcd2
+// TODO: add remaining configuration options
+var etcd2Schema = &schema.Schema{
+	Type:     schema.TypeMap,
+	Optional: true,
+	Elem: &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"discovery":                   &schema.Schema{Type: schema.TypeString, Optional: true},
+			"advertise_client_urls":       &schema.Schema{Type: schema.TypeString, Optional: true},
+			"initial_advertise_peer_urls": &schema.Schema{Type: schema.TypeString, Optional: true},
+			"listen_client_urls":          &schema.Schema{Type: schema.TypeString, Optional: true},
+			"listen_peer_urls":            &schema.Schema{Type: schema.TypeString, Optional: true},
+		},
+	},
+}
+
+// fleetSchema maps to coreos: fleet
+var fleetSchema = &schema.Schema{
+	Type:     schema.TypeMap,
+	Optional: true,
+	Elem: &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"agent_ttl":                 &schema.Schema{Type: schema.TypeInt, Optional: true},
+			"engine_reconcile_interval": &schema.Schema{Type: schema.TypeInt, Optional: true},
+			"etcd_cafile":               &schema.Schema{Type: schema.TypeString, Optional: true},
+			"etcd_certfile":             &schema.Schema{Type: schema.TypeString, Optional: true},
+			"etcd_keyfile":              &schema.Schema{Type: schema.TypeString, Optional: true},
+			"etcd_request_timeout":      &schema.Schema{Type: schema.TypeInt, Optional: true},
+			"etcd_servers": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"metadata": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeMap}, // TODO: will this allow us to put in arbitrary key/value pairs?
+			},
+			"verbosity": &schema.Schema{Type: schema.TypeInt, Optional: true},
+		},
+	},
+}
+
+// flannelSchema maps to coreos: flannel
+var flannelSchema = &schema.Schema{
+	Type:     schema.TypeMap,
+	Optional: true,
+	Elem: &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"etcd_endpoints": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"etcd_cafile":   &schema.Schema{Type: schema.TypeString, Optional: true},
+			"etcd_certfile": &schema.Schema{Type: schema.TypeString, Optional: true},
+			"etcd_keyfile":  &schema.Schema{Type: schema.TypeString, Optional: true},
+			"etcd_prefix":   &schema.Schema{Type: schema.TypeString, Optional: true},
+			"ip_masq":       &schema.Schema{Type: schema.TypeString, Optional: true},
+			"subnet_file":   &schema.Schema{Type: schema.TypeString, Optional: true},
+			"interface":     &schema.Schema{Type: schema.TypeString, Optional: true},
+			"public_ip":     &schema.Schema{Type: schema.TypeString, Optional: true},
+		},
+	},
+}
+
+// locksmithSchema maps to coreos: locksmith
+var locksmithSchema = &schema.Schema{
+	Type:     schema.TypeMap,
+	Optional: true,
+	Elem: &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"endpoint": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"etcd_cafile":   &schema.Schema{Type: schema.TypeString, Optional: true},
+			"etcd_certfile": &schema.Schema{Type: schema.TypeString, Optional: true},
+			"etcd_keyfile":  &schema.Schema{Type: schema.TypeString, Optional: true},
+			"group":         &schema.Schema{Type: schema.TypeString, Optional: true},
+			"window_start":  &schema.Schema{Type: schema.TypeString, Optional: true},
+			"window_length": &schema.Schema{Type: schema.TypeString, Optional: true},
+		},
+	},
+}
+
+// updateSchema maps to coreos: update
+var updateSchema = &schema.Schema{
+	Type:     schema.TypeMap,
+	Optional: true,
+	Elem: &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"reboot_strategy": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ValidateFunc: func(val interface{}, key string) (_ []string, errors []error) {
+					value := val.(string)
+					if value != "reboot" && value != "etcd-lock" && value != "best-effort" && value != "off" {
+						errors = append(errors, fmt.Errorf("Reboot strategy must be one of 'reboot', 'etcd-lock', 'best-effort', or 'off'"))
+					}
+
+					return
+				},
+			},
+			"server": &schema.Schema{Type: schema.TypeString, Optional: true},
+			"group":  &schema.Schema{Type: schema.TypeString, Optional: true},
+		},
+	},
+}
